36b869c537b9124485924528f6bad43a72029996
AnnaFutryk <anna.futryk@gmail.com>
2023-09-14 16:58:35 +0300

    Update MapScreen.js

--- src/Screens/MapScreen.js
+++ src/Screens/MapScreen.js
@@ -1,5 +1,41 @@
-import { Text } from "react-native";
+import MapView, { Marker } from "react-native-maps";
+import { View, StyleSheet, Dimensions } from "react-native";
 
 export const MapScreen = () => {
-  return <Text>MapScreen</Text>;
+  return (
+    <View style={styles.container}>
+      <MapView
+        style={styles.mapStyle}
+        region={{
+          latitude: 50.735677,
+          longitude: 31.38864,
+          latitudeDelta: 0.0922,
+          longitudeDelta: 0.0421,
+        }}
+        mapType="standard"
+        minZoomLevel={15}
+        onMapReady={() => console.log("Map is ready")}
+        onRegionChange={() => console.log("Region change")}
+      >
+        <Marker
+          title="I am here"
+          coordinate={{ latitude: 50.735677, longitude: 31.38864 }}
+          description="Hello"
+        />
+      </MapView>
+    </View>
+  );
 };
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#FFFFFF",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  mapStyle: {
+    width: Dimensions.get("window").width,
+    height: Dimensions.get("window").height,
+  },
+});
